e7fa3b2bdf554216aef6f7ec7a3449d5a2dea43f
Svetlana <kovalenkohp@mail.ru>
2022-01-13 08:02:09 +0400

    feat: tests passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+
+module.exports = reverse;
+
+function reverse (n) {
+	let digits = "";
+	let str = String(n);
+	for (let i = 0; i < str.length; i++) {
+		if (str[i].search(/[0-9]/) === 0) {
+			digits += str[i];
+		}
+	}
+	console.log(digits);
+	return digits.split('').reverse().join('');
+	
 }
